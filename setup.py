9a6ba5900e40f2f0fad73ad84b6d16cb4eb95286
b4stien <bastien@pectoribus.net>
2013-07-04 14:04:53 +0200

    This is 0.3.1dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mozfinance',
-    version='0.3.0',
+    version='0.3.1dev',
     packages=['mozfinance'],
     test_suite='tests',
     install_requires=['mozbase==0.3.0'],
