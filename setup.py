6d3dcbf0289c34f3b822e2759a5635c3503fb2e3
b4stien <bastien@pectoribus.net>
2013-07-18 16:20:21 +0200

    This is mozfinance v0.3.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mozfinance',
-    version='0.3.5',
+    version='0.3.6',
     packages=['mozfinance'],
     test_suite='tests',
     install_requires=['mozbase==0.3.5'],
