95291dd3366cebba0c244ea1e81b1270448bb3e0
Bastien Gandouet <bastien@mozaiqu.es>
2014-08-07 01:15:01 +0200

    Update mozbase requirement. This is mozfinance 0.5.0

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 setup(
     name='mozfinance',
-    version='0.4.1dev',
+    version='0.5.0',
     packages=['mozfinance'],
     test_suite='tests',
-    install_requires=['mozbase==0.4.0'],
+    install_requires=['mozbase==0.5.1'],
     author='Bastien GANDOUET',
     author_email="bastien@mozaiqu.es"
 )
