7a1529dfcfa4fe51309f45ad4dcaa35dafc343a5
Bastien Gandouet <bastien@mozaiqu.es>
2014-08-27 03:05:01 +0200

    This is mozfinance 0.5.4dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mozfinance',
-    version='0.5.3',
+    version='0.5.4dev',
     packages=['mozfinance'],
     test_suite='tests',
     install_requires=['mozbase==0.5.3'],
