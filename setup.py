cb624614012b55c277954bb64d952cd0cee49be7
Bastien Gandouet <dayom2705@gmail.com>
2015-09-18 02:45:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 setup(
     name='mozfinance',
-    version='0.5.5dev',
+    version='0.5.5',
     packages=['mozfinance'],
     test_suite='tests',
-    install_requires=['mozbase==0.5.3'],
+    install_requires=['mozbase==0.5.4'],
     author='Bastien GANDOUET',
     author_email="bastien@mozaiqu.es"
 )
